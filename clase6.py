787958fc19ab9da0b9f0733fc77463b64d2052cc
Daniu13 <daniel.lozano1@udea.edu.co>
2024-03-02 22:43:14 -0500

    Avances

--- clase6.py
+++ clase6.py
@@ -1,9 +1,44 @@
 class Sistema:
     def __init__(self):
-        self.__lista_mascotas = []
-        self.__num_mascotas = len(self.__lista_mascotas)
+        self.__lista_felinos = {}
+        self.__lista_caninos = {}
+        self.__num_mascotas = len(self.__lista_caninos) + len(self.__lista_felinos)
 
-    def eliminarMascota(self):
+    #Setters y Getters de la clase sistema + sus otras funciones
+    def ingresarMascota(self, mascota):
+        if mascota.verTipo().lower() == "felino":
+            self.__lista_felinos[mascota.verNumHistoria()] = mascota
+        if mascota.verTipo().lower() == "canino":
+            self.__lista_caninos[mascota.verNumHistoria()] = mascota
+    def verFechaIngreso(self, historia):
+        if self.verificar(historia)[1]:
+            return self.__lista_caninos[historia].verFechaIngreso()
+        elif self.verificar(historia)[1] == False:
+            return self.__lista_felinos[historia].verFechaIngreso()
+        return None
+    def verNumeroMascotas(self):
+        return len(self.__lista_felinos) + len(self.__lista_caninos)
+    def verMedicamento(self, historia):
+        if self.verificar(historia)[1]:
+            return self.__lista_caninos[historia].verMedicamentos()
+        elif self.verificar(historia)[1] == False:
+            return self.__lista_felinos[historia].verMedicamentos()
+        return None
+    def eliminarMascota(self, historia):
+        if self.verificar(historia)[1]:
+            self.__lista_caninos.pop(historia)
+            return True
+        elif self.verificar(historia)[1] == False:
+            self.__lista_felinos.pop(historia)
+            return True
+        return False
+    def verificar(self, historia):
+        if historia in self.__lista_caninos:
+            return True, True
+        if historia in self.__lista_felinos:
+            return True, False
+        return False
+    def salir():
         pass
 
 class Mascota:
@@ -13,7 +48,7 @@ class Mascota:
         self.__tipo = ""
         self.__peso = 0
         self.__fecha_ingreso = ""
-        self.__medicamento = ""
+        self.__lista_medicamente = []
 
     #Setters de Mascota
     def asignarNombre(self, n):
@@ -26,8 +61,8 @@ class Mascota:
         self.__peso = p
     def asignarFechaIngreso(self, fi):
         self.__fecha_ingreso = fi
-    def asignarMedicamento(self, m):
-        self.__medicamento = m
+    def asignarMedicamentos(self, m):
+        self.__lista_medicamente = m
 
     #Getters de Mascota
     def verNombre(self):
@@ -40,8 +75,8 @@ class Mascota:
         return self.__peso
     def verFechaIngreso(self):
         return self.__fecha_ingreso
-    def verMedicamento(self):
-        return self.__medicamento
+    def verMedicamentos(self):
+        return self.__lista_medicamente
     
 class Medicamente:
     def __init__(self):
@@ -59,3 +94,9 @@ class Medicamente:
         return self.__nombre
     def verDosis(self):
         return self.__dosis
+
+
+
+
+if __name__ == "__main__":
+    pass
